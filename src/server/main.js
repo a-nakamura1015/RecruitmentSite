79e4c76fcb06efa5694f12d3640d7349e3b06c56
a-nakamura1015 <a-nakamura@esm.co.jp>
2019-11-05 02:11:03 +0900

    ユーザー新規登録画面の骨組みを作成した

--- src/server/main.js
+++ src/server/main.js
@@ -1,3 +1,11 @@
-function doGet() {
+function doGet(e) {
+  var page = e.parameter['page'];
+  if (page == 'signUp') return HtmlService.createTemplateFromFile('signUp').evaluate().setTitle('ユーザー新規登録');
   return HtmlService.createTemplateFromFile('login').evaluate().setTitle('ログイン');
+}
+
+function getUrl(param) {
+  var url = ScriptApp.getService().getUrl();
+  if (param) url += param;
+  return url;
 }
