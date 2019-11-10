1577bf738ba3385d02dfe58d521d50a7a020dd19
a-nakamura1015 <a-nakamura@esm.co.jp>
2019-11-10 19:25:47 +0900

    イベント詳細画面の骨組みを作成した

--- src/server/main.js
+++ src/server/main.js
@@ -2,6 +2,7 @@ function doGet(e) {
   var page = e.parameter['page'];
   if (page == 'signUp') return HtmlService.createTemplateFromFile('signUp').evaluate().setTitle('ユーザー新規登録');
   if (page == 'myPage') return HtmlService.createTemplateFromFile('myPage').evaluate().setTitle('マイページ');
+  if (page == 'eventDetail') return HtmlService.createTemplateFromFile('eventDetail').evaluate().setTitle('イベント詳細');
   return HtmlService.createTemplateFromFile('login').evaluate().setTitle('ログイン');
 }
 
