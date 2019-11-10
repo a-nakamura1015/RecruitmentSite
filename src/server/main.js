6555e7be5501b7748acc7b4aa3aba85e4d52a165
a-nakamura1015 <a-nakamura@esm.co.jp>
2019-11-10 20:46:47 +0900

    ログイン画面から新規登録画面へ遷移できるようにした

--- src/server/main.js
+++ src/server/main.js
@@ -1,9 +1,25 @@
 function doGet(e) {
   var page = e.parameter['page'];
-  if (page == 'signUp') return HtmlService.createTemplateFromFile('signUp').evaluate().setTitle('ユーザー新規登録');
-  if (page == 'myPage') return HtmlService.createTemplateFromFile('myPage').evaluate().setTitle('マイページ');
-  if (page == 'eventDetail') return HtmlService.createTemplateFromFile('eventDetail').evaluate().setTitle('イベント詳細');
-  return HtmlService.createTemplateFromFile('login').evaluate().setTitle('ログイン');
+  var fileName = '';
+  var title = '';
+  if (page == 'signUp') {
+    // 遷移先がユーザー新規登録画面の場合
+    fileName = 'signUp';
+    title = 'ユーザー新規登録';
+  } else if (page == 'myPage') {
+    // 遷移先がマイページ画面の場合
+    fileName = 'myPage';
+    title = 'マイページ';
+  } else if (page == 'eventDetail') {
+    // 遷移先がイベント詳細画面の場合
+    fileName = 'eventDetail';
+    title = 'イベント詳細';
+  } else if (page == '') {
+    // 遷移先が指定されていない場合
+    fileName = 'login';
+    title = 'ログイン';
+  }
+  return HtmlService.createTemplateFromFile(fileName).evaluate().setTitle(title).setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
 
 function getUrl(param) {
